373b1faa887b2003eb825026b2a0ac638f3717fd
Antoine Tissier <ati@activeviam.com>
2021-09-16 18:05:31 +0200

    More minimalist

--- app2/src/App.js
+++ app2/src/App.js
@@ -1,5 +1,4 @@
 import { NameContext } from "@shared-context/shared-library/src/context/NameContext";
-import { bar } from "@shared-context/shared-library/src/bar/bar";
 import React from "react";
 import Welcome from "./Welcome";
 
@@ -7,7 +6,6 @@ const App = () => (
   <div>
     <h1>Context Provider</h1>
     <h2>App 2</h2>
-    <h3>{bar}</h3>
     <NameContext.Provider value="Susan">
       <Welcome />
     </NameContext.Provider>
--- shared-library/src/index.js
+++ shared-library/src/index.js
@@ -1,3 +1,1 @@
 export { NameContext } from "./context/NameContext";
-export { foo } from "./foo/foo";
-export { bar } from "./bar/bar";
