e0a6e8be84fae409b0d4a67aedd867ebd09885a0
Antoine Tissier <ati@activeviam.com>
2021-09-16 18:05:31 +0200

    More minimalist

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,5 +1,4 @@
 import { NameContext } from "@shared-context/shared-library/src/context/NameContext";
-import { foo } from "@shared-context/shared-library/src/foo/foo";
 import React from "react";
 
 const Welcome = React.lazy(() => import("app2/Welcome"));
@@ -8,7 +7,6 @@ const App = () => (
   <div>
     <h1>Context Provider</h1>
     <h2>App 1</h2>
-    <h3>{foo}</h3>
     <NameContext.Provider value="Billy">
       <React.Suspense fallback="Loading Name">
         <Welcome />
